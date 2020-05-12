9b3e78233e3e405aaeeeaedea421c34ee31c0560
omh02033 <omh02033@gmail.com>
2020-05-12 20:23:33 +0900

    V0.03#2

--- routes/track.js
+++ routes/track.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const mysql = require('mysql');
+const path = require('path');
 
 let conn = mysql.createConnection({
     host: 'localhost',
@@ -10,7 +11,7 @@ let conn = mysql.createConnection({
     database: 'delivery'
 });
 conn.connect();
-
+/*
 let code = ['99', '37', '29',     // 국제 택배
             '38', '42', '57',
             '13', '33', '12',
@@ -26,8 +27,11 @@ let code = ['99', '37', '29',     // 국제 택배
             '04', '46', '24',
             '56', '30', '44',
             '64', '58'];
+            */
+let code = ['05'];
 
-router.post('/', (req, res) => {
+router
+.post('/', (req, res) => {
     console.log(code.length);
     for(let i=0; i<code.length; i++) {
         axios.get('http://info.sweettracker.co.kr/api/v1/trackingInfo', {
@@ -37,34 +41,78 @@ router.post('/', (req, res) => {
                 t_invoice: req.body.num
             }
         }).then(function (response) {
-            console.log(response.data.code + " : " + code[i]);
-            if(response.data.code == 200) {
-                res.status(200).json({ msg: response.data.msg });
-                let sql = 'SELECT lookup FROM top WHERE code=?';
-                conn.sql(sql, [code[i]], (err, data) => {
-                    if(err) {
-                        res.status(400).json({ msg: `조회 추가과정에서 에러가 발생했습니다 : ${code[i]}` });
-                    } else {
-                        let look = data + 1;
-                        let sq = 'UPDATE top SET lookup=? WHERE code=?';
-                        conn.sql(sq, [look, code[i]], (err, result, fields) => {
-                            if(err) {
-                                res.status(400).json({ msg: '조회 변경 과정에서 에러가 발생했습니다.' });
-                            }
-                        })
-                    }
+            console.log(`${i} : ${code[i]}`);
+            if(response.data.senderName) {
+                console.log(response.data.level);
+                let detailIndex = response.data.trackingDetails.length;
+                console.log(response.data.trackingDetails[2]);
+                
+                res.status(200).json({
+                    msg: 'success',
+                    code: response.data.invoiceNo,
+                    uname: response.data.receiverName,
+                    itemName: response.data.itemName,
+                    // where: response.data.trackingDetails[response.data.trackingDetails.length].where,
+                    level: response.data.level,
+                    t_code: code[i]
                 });
+                // let sql = 'SELECT lookup FROM top WHERE code=?';
+                // console.log(code[i]);
+                // conn.query(sql, [code[i]], (err, data) => {
+                //     console.log("data" + data);
+                //     if(err) {
+                //         res.status(400).json({ msg: `조회 추가과정에서 에러가 발생했습니다 : ${code[i]}` });
+                //     } else {
+                //         let look = data + 1;
+                //         let sq = 'UPDATE top SET lookup=? WHERE code=?';
+                //         console.log(look, code[i]);
+                //         conn.query(sq, [look, code[i]], (err, result, fields) => {
+                //             if(err) {
+                //                 res.status(400).json({ msg: '조회 변경 과정에서 에러가 발생했습니다.' });
+                //             } else {
+                //                 res.status(200).json({ msg: '.`success`.' });
+                //             }
+                //         })
+                //     }
+                // });
                 return;
             } else {
                 if(i >= code.length - 1) {
                     console.log('fail');
-                    console.log(response.data.msg);
-                    res.status(400).json({ msg: response.data.msg });
+                    res.status(400).json({ msg: '조회를 하지 못하였습니다.', alt: response.data.msg });
                     return;
                 }
             }
         })
     }
-});
+})
+
+.get('/detail', (req,res) => {
+    res.sendFile('track/detail.html', { root: path.join(__dirname, '../public/html') });
+})
+
+.post('/detail', (req, res) => {
+    axios.get('http://info.sweettracker.co.kr/api/v1/trackingInfo', {
+        params: {
+            t_key: '5yH0MNx3JNyytFrtB1D2gg',
+            t_code: req.body.t_code,
+            t_invoice: req.body.t_invoice
+        }
+    }).then(function (response) {
+        if(response.data.senderName) {
+            res.status(200).json({
+                code: response.data.invoiceNo,
+                uname: response.data.receiverName,
+                itemName: response.data.itemName,
+                where: whre,
+                level: response.data.level,
+                t_code: code[i]
+            });
+        } else {
+            console.log('fail');
+            res.status(400).json({ msg: '조회를 하지 못하였습니다.' });
+        }
+    })
+})
 
 module.exports = router;
